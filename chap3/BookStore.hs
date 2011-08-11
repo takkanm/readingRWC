6d8a188f0090ae46f2e946d61abed003368cb0c2
takkanm <takkanm@gmail.com>
2011-08-11 20:17:05 +0900

    create type

--- chap3/BookStore.hs
+++ chap3/BookStore.hs
@@ -5,3 +5,12 @@ data MagazineInfo = Magazine Int String [String]
 					deriving (Show)
 
 myInfo = Book 9780135072455 "Algebra of Programming" ["Richard Bird", "Oege de Moor"]
+
+data BookReview = BookReview BookReview CustomerID String
+
+type CustomerID = Int
+type ReviewBody = String
+
+data BetterReview = BetterReview BookInfo CustomerID ReviewBody
+
+type BookRecord = (BookInfo, BookReview)
